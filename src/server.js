af2ce77b6868a76d7fc24565d24fca87e1c641da
Pham Minh Kiet <minhkiet3466@gmail.com>
2023-10-30 08:17:37 +0700

    update

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const express = require('express')
 const app = express()
 const configViewEngine = require('./config/viewEngine')
 const webRoute = require('./routes/web')
-// get the client
-const mysql = require('mysql2');
+const connection = require('./config/dataBase')
 
 let port = process.env.PORT || 8080
 let hostname = process.env.HOST_NAME
@@ -14,19 +13,12 @@ app.use('/', webRoute)
 
 //test connection
 // create the connection to database
-const connection = mysql.createConnection({
-     host: 'localhost',
-     port: 3307,
-     user: 'root',
-     password: '123456',
-     database: 'minhkiet'
-});
+
 
 connection.query(
      'SELECT * FROM Users u ',
      function (err, results, fields) {
           console.log('>>>results: ', results); // results contains rows returned by server
-          console.log('>>>fields: ', fields); // fields contains extra meta data about results, if available
      }
 );
 
